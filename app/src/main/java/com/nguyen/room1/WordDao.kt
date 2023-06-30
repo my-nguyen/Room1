b6191b640923614285361ddda075a0c62418258b
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-64G-2T.local>
2023-06-30 13:01:22 -0700

    b3: Observing database changes

--- app/src/main/java/com/nguyen/room1/WordDao.kt
+++ app/src/main/java/com/nguyen/room1/WordDao.kt
@@ -4,11 +4,12 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WordDao {
     @Query("SELECT * FROM words ORDER BY word ASC")
-    fun getAlphabetizedWords(): List<Word>
+    fun getAlphabetizedWords(): Flow<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
