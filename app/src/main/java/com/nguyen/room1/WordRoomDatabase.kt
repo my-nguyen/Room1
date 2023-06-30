aac5d7e9e820950f6d38f1305f380d168c565c34
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-64G-2T.local>
2023-06-30 16:11:38 -0700

    b10: Populate the database

--- app/src/main/java/com/nguyen/room1/WordRoomDatabase.kt
+++ app/src/main/java/com/nguyen/room1/WordRoomDatabase.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 // Annotates class to be a Room Database with a table (entity) of the Word class
 @Database(entities = [Word::class], version = 1, exportSchema = false)
-public abstract class WordRoomDatabase : RoomDatabase() {
+abstract class WordRoomDatabase : RoomDatabase() {
 
     abstract fun wordDao(): WordDao
 
@@ -16,18 +19,47 @@ public abstract class WordRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        // Room database operations can't be done on the UI thread; must launch a coroutine on the
+        // IO Dispatcher
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
             // if the INSTANCE is not null, then return it. if it is, then create the database
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_database"
-                ).build()
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
                 // return instance
                 instance
             }
+        }
+    }
+
+    // custom implementation of the RoomDatabase.Callback()
+    private class WordDatabaseCallback(private val scope: CoroutineScope) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao) {
+            // Delete all content here.
+            wordDao.deleteAll()
+
+            // Add sample words.
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
+
+            // TODO: Add your own words!
         }
     }
 }
--- app/src/main/java/com/nguyen/room1/WordsApplication.kt
+++ app/src/main/java/com/nguyen/room1/WordsApplication.kt
@@ -1,8 +1,14 @@
 package com.nguyen.room1
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class WordsApplication : Application() {
+    // this scope is used for populating the database. It's related to the app's lifecycle and not
+    // something like viewModelScope
+    val applicationScope = CoroutineScope(SupervisorJob())
+
     // Using by lazy so the database and the repository are only created when they're needed rather
     // than when the application starts
     val database by lazy { WordRoomDatabase.getDatabase(this) }
