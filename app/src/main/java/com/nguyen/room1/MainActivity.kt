084bcaef7b44428c5fe43cf25cdb06cd7c77533e
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-64G-2T.local>
2023-06-30 16:58:51 -0700

    b12: Connect with the data

--- app/src/main/java/com/nguyen/room1/MainActivity.kt
+++ app/src/main/java/com/nguyen/room1/MainActivity.kt
@@ -1,17 +1,50 @@
 package com.nguyen.room1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.nguyen.room1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
+    private val code = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val wordsAdapter = WordsAdapter()
         binding.recyclerview.apply {
-            adapter = WordsAdapter()
+            adapter = wordsAdapter
             layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, NewWordActivity::class.java)
+            startActivityForResult(intent, code)
+        }
+
+        wordViewModel.allWords.observe(this) { words ->
+            // Update the cached copy of the words in the adapter
+            words?.let {
+                wordsAdapter.submitList(it)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == code && resultCode == RESULT_OK) {
+            data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let {
+                wordViewModel.insert(Word(it))
+            }
+        } else {
+            Toast.makeText(applicationContext, R.string.empty_not_saved, Toast.LENGTH_LONG).show()
         }
     }
 }
--- app/src/main/java/com/nguyen/room1/WordsApplication.kt
+++ app/src/main/java/com/nguyen/room1/WordsApplication.kt
@@ -11,6 +11,6 @@ class WordsApplication : Application() {
 
     // Using by lazy so the database and the repository are only created when they're needed rather
     // than when the application starts
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
