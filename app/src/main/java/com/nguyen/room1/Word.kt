098788db285a2ddfdb357dca65bf5a005cc624f4
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-64G-2T.local>
2023-06-30 17:04:08 -0700

    removed id column

--- app/src/main/java/com/nguyen/room1/Word.kt
+++ app/src/main/java/com/nguyen/room1/Word.kt
@@ -5,6 +5,4 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "words")
-class Word(@PrimaryKey(autoGenerate = true) val id: Int,
-           @ColumnInfo(name = "word") val word: String
-           )
+class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
